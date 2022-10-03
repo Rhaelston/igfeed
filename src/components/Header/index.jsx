5f5aafd35ba253f0604831306a2412012045c8c4
Rhaelston Honório <rhaelston@hotmail.com>
2022-10-03 01:26:46 -0300

    igfeed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,6 +6,7 @@ export function Header () {
     return (
         <header className={styles.header}>
             <img src={igniteLogo} alt="logotipo do ignite"/>
+            <h1>igfeed</h1>
         </header>
     );
 }
